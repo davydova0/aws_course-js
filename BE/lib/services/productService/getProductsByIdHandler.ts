7fb3969b70de6145851e1291d3cc2231ed026108
Davydova Olha <olha_davydova@epam.com>
2024-10-21 19:18:02 +0300

    task 4

--- BE/lib/services/productService/getProductsByIdHandler.ts
+++ BE/lib/services/productService/getProductsByIdHandler.ts
@@ -1,15 +1,46 @@
-import * as path from "node:path";
-import * as fs from "node:fs";
+import {BatchGetItemCommand, DynamoDBClient} from "@aws-sdk/client-dynamodb";
 
-const filePath = path.join(__dirname, "products-mock-data.json")
-const rawData = fs.readFileSync(filePath, "utf8")
-const products = JSON.parse(rawData);
+const dynamoDB = new DynamoDBClient({region: "us-east-1"});
+
+const getParams = (id: string) => ({
+    RequestItems: {
+        "Products": {
+            "Keys": [
+                {id: {S: id}}
+            ]
+        },
+        "Stock": {
+            "Keys": [{
+                "product_id": {S: id}
+            }]
+        }
+    }
+})
 
 // @ts-ignore
-export async function handler(event){
-    const product = products.find((p: { id: any; }) => p.id == event.pathParameters.productId)
-    return {
-        body: JSON.stringify(product),
-        statusCode: 200,
-    };
+export async function handler(event) {
+    try {
+        const params = getParams(event.pathParameters.productId);
+
+        // @ts-ignore
+        const command = new BatchGetItemCommand(params);
+        const response = await dynamoDB.send(command);
+        return {
+            body: JSON.stringify(response.Responses),
+            statusCode: 200,
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET, POST, DELETE, OPTIONS',
+                'Access-Control-Allow-Headers': 'X-Requested-With, Content-Type, Accept, Authorization',
+            }
+        };
+    } catch (err) {
+        // @ts-ignore
+        const {message} = err;
+        return {
+            message: message,
+            statusCode: 500,
+        };
+    }
 }
